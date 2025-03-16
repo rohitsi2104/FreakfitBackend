af09b16f7a9b4a4924081493bbcdfe7c36cf9e54
Prashant Sharma <prashantsharmafeb11@gmail.com>
2025-03-16 11:35:13 +0530

    updated setting.py6

--- freakfitbackend/settings.py
+++ freakfitbackend/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+538bbe7-bh-0c23_rjtq#p$$7ct5%b=bw5)+745fizpbo*2hc'
+# SECRET_KEY = 'django-insecure-+538bbe7-bh-0c23_rjtq#p$$7ct5%b=bw5)+745fizpbo*2hc'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,9 +140,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
