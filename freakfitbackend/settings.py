54bff3259ca8fc272bdf14873eb957ac49abce5b
Prashant Sharma <prashantsharmafeb11@gmail.com>
2025-03-27 18:02:21 +0530

    change in setting and requirements.txt json

--- freakfitbackend/settings.py
+++ freakfitbackend/settings.py
@@ -152,10 +152,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#
+# STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles" # ✅ Correct location
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles" # ✅ Correct location
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+if not DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
