7e3b310afb080af29b90776a479baef43410d174
Prashant Sharma <prashantsharmafeb11@gmail.com>
2025-03-27 20:12:08 +0530

    change in setting and requirements.txt json2

--- freakfitbackend/settings.py
+++ freakfitbackend/settings.py
@@ -152,19 +152,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#
-# STATIC_URL = "/static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles" # ✅ Correct location
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-if not DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
--- freakfitbackend/urls.py
+++ freakfitbackend/urls.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path, include
 
 
@@ -6,3 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('customers.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_ROOT, documents_root= settings.STATIC_ROOT)
